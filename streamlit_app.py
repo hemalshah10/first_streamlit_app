4d63c7d7b959d2d00ddba76a50f4e5766f685ee6
hemalshah10 <44573629+hemalshah10@users.noreply.github.com>
2023-04-24 11:43:07 +0100

    Update streamlit_app.py
    Added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
