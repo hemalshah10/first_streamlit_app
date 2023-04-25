5a46937c9e054a0585872683221ac149820f8837
hemalshah10 <44573629+hemalshah10@users.noreply.github.com>
2023-04-25 22:49:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,16 +24,17 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header ('Fruityvice Fruit Advice')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-
-# normalize the json using pandas
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# return the normalize data in a table
-streamlit.dataframe(fruityvice_normalized)
+streamlit.header ('Fruityvice Fruit Advice!')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice
+    streamlit.error("Please select a fruit to get information")
+  else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 
