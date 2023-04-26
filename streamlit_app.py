4974472a09e2438cc9ece65afdb0b9dbee546b08
hemalshah10 <44573629+hemalshah10@users.noreply.github.com>
2023-04-26 11:33:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,6 +64,7 @@ def insert_row_snowflake(new_fruit):
          return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a fruit to the List'):
                                      
 streamlit.stop()
 #streamlit.write('Thanks for adding ', add_my_fruit)
